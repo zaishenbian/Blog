40a10e7e6b34a00c722659f1133c0e39de8ccb1f
zaishenbian <779458847@qq.com>
2018-07-12 23:47:41 +0800

    layui引入

--- routers/main.js
+++ routers/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('index');
+    res.render('./main/index');
 })
 
 router.get('/user', function(req, res, next){
