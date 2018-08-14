c4d97907a70ac68f4b1b8266a1a44903e803593e
zaishenbian <779458847@qq.com>
2018-08-14 23:53:15 +0800

    后台管理页面布局

--- routers/admin.js
+++ routers/admin.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 //登录注册页面路由
+router.get('/', function(req, res, next){
+    res.render('admin/index');
+})
+
 router.get('/login', function(req, res, next){
     res.render('admin/login');
 })
