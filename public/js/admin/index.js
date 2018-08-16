bcc5525f8e4409510ffff4ff73f6514755e0cf13
zaishenbian <779458847@qq.com>
2018-08-16 23:59:03 +0800

    代碼格式化

--- public/js/admin/index.js
+++ public/js/admin/index.js
@@ -1,36 +1,36 @@
 //JavaScript代码区域
-layui.use('element', function(){
-    var element = layui.element;
-    var $ = layui.jquery;
-    var tabs = [];   //
-    
-    //监听左侧导航
-    element.on("nav(nav)", function(elem){
+layui.use('element', function () {
+  var element = layui.element;
+  var $ = layui.jquery;
+  var tabs = [];   //
+
+  //监听左侧导航
+  element.on("nav(nav)", function (elem) {
     let title = elem[0].innerHTML;
     let layId = elem[0].innerText;
     //防止tab重复添加
-    if(tabs.indexOf(layId)>-1){
-        element.tabChange('tab', layId);
-        return;
-    }else{
-        let content = title;
-        $.ajax({
-        url:'/api/'
-        })
-        //添加tab
-        element.tabAdd('tab', {
+    if (tabs.indexOf(layId) > -1) {
+      element.tabChange('tab', layId);
+      return;
+    } else {
+      let content = title;
+      $.ajax({
+        url: '/api/'
+      })
+      //添加tab
+      element.tabAdd('tab', {
         title: title
-        ,content: content //支持传入html
-        ,id: layId
-        }); 
-        tabs.push(layId);
-        element.tabChange('tab', layId);
+        , content: content //支持传入html
+        , id: layId
+      });
+      tabs.push(layId);
+      element.tabChange('tab', layId);
     }
-    })
+  })
 
-    //监听tab关闭
-    element.on('tabDelete(tab)', function(data){
+  //监听tab关闭
+  element.on('tabDelete(tab)', function (data) {
     var index = data.index;
-    tabs.splice(index-1, 1);
-    })
+    tabs.splice(index - 1, 1);
+  })
 });
